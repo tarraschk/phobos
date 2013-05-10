ccbca3c26001d039979669985fa9bba17d333f23
arthur-feral <arthur.feral@hotmail.fr>
2013-05-10 15:51:38 +0200

    changes Ship

--- js/classes/Bot.js
+++ js/classes/Bot.js
@@ -10,17 +10,11 @@
 	Bot.path = 'img/ship/';
 
 // public properties:
-	b.position = {x:null, y:null, rotation: 90};
-	b.destination = {x:null, y:null};
-	b.limitSpeed = 5.5;
-	b.acceleration = 0.08 ; 
-	b.limitRotation;
-	b.currentSpeed = 0 ; 
-	b.rotationSpeed = 3;
-	b.hasDestination = false;
-	b.name;
+	b.hasTarget = false;
 // constructor:
-
+	b.checkForTarget = function(){
+		
+	}
 
 // public methods:
 	window.Bot = Bot;
