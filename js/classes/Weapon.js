697ce73f2722e09bec524f36bb419e2c81e86244
RaphSfeir <raphael.sfeir@eleves.ec-nantes.fr>
2013-05-20 22:55:26 +0200

    Prise en compte arme.

---
+++ js/classes/Weapon.js
@@ -0,0 +1,43 @@
+(function (window) {
+
+	Weapon = function(){
+		this.initialize();
+	}
+
+	var w = Weapon.prototype ;
+
+// static public properties:
+	Weapon.path = 'img/objects/stations/';
+	
+// public properties:
+	w._power = null // dommages que la station peut causer quand elle attaque;
+	w._id = null;
+	w._range = 350 ; 
+	w._ready = false;
+// constructor:
+	w.initialize = function () {
+		this._id = utils.generateId();
+		this._power = 20;
+	}
+
+// public methods:
+	w.tick = function (event) {
+		if (Math.random() < 0.1) this._ready = true;
+		else this._ready = false ; 
+	}
+	
+	w.getRange = function() {
+		return this._range ; 
+	}
+
+	w.isReady = function() {
+		return this._ready;
+	}
+
+	w.setReady = function (newReady) {
+		this._ready = newReady; 
+	}
+
+	window.Weapon = Weapon;
+
+}(window));
