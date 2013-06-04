0adf9e69b4cc922ab917fae6357815da3ca2fd46
RaphSfeir <raphael.sfeir@eleves.ec-nantes.fr>
2013-06-05 00:38:28 +0200

    Boucle avec prise en compte FPS.

--- core/assets/Game.js
+++ core/assets/Game.js
@@ -31,8 +31,10 @@
 // public methods:
 
 	g.tick = function (event) {
+		t  = Date.now() ; 
 	    //Work out the delta time
-	    this.dt = this.lastframetime ? ( (t - this.lastframetime)/1000.0).fixed() : 0.016;
+	    this.dt = this.lastframetime ? ( (t - this.lastframetime)/1000.0) : 0.016;
+		console.log(this.dt);
 
 	        //Store the last frame time
 	    this.lastframetime = t;
@@ -41,19 +43,19 @@
 	}
 	g.objectsTick = function() {
 		allowMoveClick = true ; 
-		for (key in g._shipsList) {
-			if (String(Number(key)) === key && g._shipsList.hasOwnProperty(key)) {
-				if (g._shipsList[key].index == g._shipsList[key].id) g._shipsList[key].tick();
-			}
-		}
-		// g._gameGraphics.tick();
-		// g._camera.tick();
-		g._station1.tick();
-		g._station2.tick();
-		g._shipsList[3].moveTo({x:-150,y:-200});
-		for (var k = 0 ; k < g._tilesMap.length ; k++) {
-			g._tilesMap[k].tick();
-		}
+		// for (key in g._shipsList) {
+		// 	if (String(Number(key)) === key && g._shipsList.hasOwnProperty(key)) {
+		// 		if (g._shipsList[key].index == g._shipsList[key].id) g._shipsList[key].tick();
+		// 	}
+		// }
+		// // g._gameGraphics.tick();
+		// // g._camera.tick();
+		// g._station1.tick();
+		// g._station2.tick();
+		// g._shipsList[3].moveTo({x:-150,y:-200});
+		// for (var k = 0 ; k < g._tilesMap.length ; k++) {
+		// 	g._tilesMap[k].tick();
+		// }
 		// renderCanvas();
 		// backgroundGame.tick();
 		// backgroundGame2.tick();
