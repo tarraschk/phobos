f42731a9101ff106ca5e6673e2c9f508028d960d
RaphSfeir <raphael.sfeir@eleves.ec-nantes.fr>
2013-06-27 01:03:58 +0200

    Firebase/

--- firebase.server.js
+++ firebase.server.js
@@ -1,3 +1,3 @@
 var Firebase = require('firebase');
-var myRootRef = new Firebase('https:/phobosdb.firebaseIO.com/');
+var myRootRef = new Firebase('https://phobosdb.firebaseIO.com/');
 myRootRef.set("hello world!");
