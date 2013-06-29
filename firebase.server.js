52d8e788815c069bd880ef1da1523544188fa3e3
RaphSfeir <raphael.sfeir@eleves.ec-nantes.fr>
2013-06-29 18:40:03 +0200

    Coucou.

--- firebase.server.js
+++ firebase.server.js
@@ -4,15 +4,6 @@ phobosAlpha.set({server:{game: {sector1:"data", sector2: "coucou"} } });
 
 setInterval(function(){
 	var sharedData = server.getGame().getSharedData();
-	console.log("SHAREDDATA");
-	console.log("SHAREDDATA");
-	console.log("SHAREDDATA");
-	console.log("SHAREDDATA");
-	console.log("SHAREDDATA");
-	console.log(sharedData);	
-	console.log("SHAREDDATA");
-	console.log("SHAREDDATA");
-	console.log("SHAREDDATA");
 
 	phobosAlpha.set( {server:{game: { sector1: sharedData}}} );
 
