3d7f0895b7f307adc4319fd35572b7ca860c6617
arthur-feral <arthur.feral@hotmail.fr>
2013-07-01 22:37:45 +0200

    Update UI.js

--- client/js/assets/UI.js
+++ client/js/assets/UI.js
@@ -59,21 +59,31 @@
 		this._container.addChild(s);	
 	}
 	ui.drawSurround = function(target) {
-		var g = new _.Bitmap("img/ui/surround.png");
-		var surroundDimensions = {w:296,h:167};
-		g.x = target.x + client.getGame().getCamera().x();
-		g.y = target.y + client.getGame().getCamera().y();
-		if (target.image) {
-		console.log(target.image.width );
-		console.log(target.image.height );
-			g.scaleX = 1.2 * target.image.width / surroundDimensions.w;
-			g.scaleY = 1.1 * target.image.height / surroundDimensions.h;
-		}
-		else if (target.spriteSheet) {
-			console.log(target.spriteSheet);
-			g.scaleX = 1.2 * target.spriteSheet._regX / surroundDimensions.w;
-			g.scaleY = 1.1 * target.spriteSheet._regY / surroundDimensions.h;
-		}
+		// var g = new _.Bitmap("img/ui/surround.png");
+		// var surroundDimensions = {w:296,h:167};
+		// g.x = target.x + client.getGame().getCamera().x();
+		// g.y = target.y + client.getGame().getCamera().y();
+		// if (target.image) {
+		// console.log(target.image.width );
+		// console.log(target.image.height );
+		// 	g.scaleX = 1.2 * target.image.width / surroundDimensions.w;
+		// 	g.scaleY = 1.1 * target.image.height / surroundDimensions.h;
+		// }
+		// else if (target.spriteSheet) {
+		// 	console.log(target.spriteSheet);
+		// 	g.scaleX = 1.2 * target.spriteSheet._regX / surroundDimensions.w;
+		// 	g.scaleY = 1.1 * target.spriteSheet._regY / surroundDimensions.h;
+		// }
+
+		var g = new _.Graphics();
+	    g.setStrokeStyle(5);
+	    g.beginStroke("#069D1A");
+
+	    g.drawEllipse(target.x + client.getGame().getCamera().x() , target.y + client.getGame().getCamera().y(), target.image.width, target.image.height);
+	    g.endStroke();
+	    var s = new _.Shape(g);
+	    this._container.addChild(s);
+
 		// g.beginStroke(_.Graphics.getRGB(50,6,10));
 		// g.setStrokeStyle(5,10,10);
 		// g.drawCircle(target.x ,target.y ,300);
@@ -83,7 +93,7 @@
 		// .lineTo(endPoint.x,endPoint.y).endStroke();
 
 		// var s = new _.Shape(g);
-		this._container.addChild(g);
+		// this._container.addChild(g);
 	}
 
 	ui.showEntityInfos = function(entity){
